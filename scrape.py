57bbf9eff5edf1b2952615d65a251ecf6952ffaf
Hunter Kuffel <hkuffel@alumni.nd.edu>
2019-10-16 20:36:33 -0400

    changed chromedriver path

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 from splinter import Browser
 
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
+executable_path = {'executable_path': '/venv/bin/chromedriver'}
 browser = Browser('chrome', **executable_path, headless=False)
 
 def news():
